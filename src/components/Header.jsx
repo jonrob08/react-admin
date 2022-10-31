56623557f4d67271388ce9ff43b5a10e46b3bbc0
jonrob <jonrob08@gmail.com>
2022-10-31 10:44:54 -0700

    updated header to include typography defaults and passed in title and subtitle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,23 @@ const Header = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
     return (
-        <Box>
-            
+        <Box mb="30px">
+            <Typography
+                variant="h2"
+                color={colors.grey[100]}
+                fontWeight='bold'
+                sx={{ mb: '5px' }}
+            >
+                {title}
+            </Typography>
+            <Typography
+                variant="h2"
+                color={colors.grey[100]}
+                fontWeight='bold'
+                sx={{ mb: '5px' }}
+            >
+                {subtitle}
+            </Typography>
         </Box>
     )
 }
