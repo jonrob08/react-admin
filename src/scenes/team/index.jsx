afffdfd22056b2c83ecdeca5c65c778c73988f59
jonrob <jonrob08@gmail.com>
2022-10-31 13:34:43 -0700

    created Team page and added boilerplate component and imported everything needed

---
+++ src/scenes/team/index.jsx
@@ -0,0 +1,18 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import { mockDataTeam } from '../../data/mockData'
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined"
+import LockOpenSettingsOutlinedIcon from "@mui/icons-material/LockOpenSettingsOutlined"
+import SecuritySettingsOutlinedIcon from "@mui/icons-material/SecuritySettingsOutlined"
+import Header from "../../components/Header";
+
+const Team = () => {
+    return (
+        <Box>
+            <h1>Team</h1>
+        </Box>
+    )
+}
+
+export default Team
